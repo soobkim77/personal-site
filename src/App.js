07418904f0aa162e9c70d15add085be1a46397de
Soo-Beom <soobeomkim77@gmail.com>
2021-06-08 17:49:24 -0700

    set up declartive routing with react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
+import { Switch, Route } from 'react-router-dom'
 import NavBar from './components/NavBar'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import main from './images/logo.jpg'
 
 function App() {
   return (
     <div>
-      <NavBar />
+      <Switch>
+        <Route exact path="/" component={NavBar}/>
+      </Switch>
     </div>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,26 +1,13 @@
 import React from 'react';
-import { Navbar, Nav } from 'react-bootstrap'
-import logo from '../images/initial-logo.png';
+import '../App.css';
+import { Link } from 'react-router-dom'
 
 const NavBar = () => {
     return (
-        <Navbar bg="white" variant="light">
-            <Navbar.Brand>
-                <img 
-                    src={logo}
-                    width="100"
-                    height="100"
-                    className="d-inline-block align-top"
-                    alt="Personal Logo"
-                />
-                Soo-Beom Kim
-            </Navbar.Brand>
-            <Nav className="mr-auto">
-                <Nav.Link href="#home">Home</Nav.Link>
-                <Nav.Link href="#features">Features</Nav.Link>
-                <Nav.Link href="#pricing">Pricing</Nav.Link>
-            </Nav>
-        </Navbar>
+        <div>
+            <Link>Home</Link>
+        </div>
+
     )
 }
 
