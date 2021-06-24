d7d515014304fe316b7eef10460ca49e8bcbf0d4
Soo-Beom <soobeomkim77@gmail.com>
2021-06-23 17:38:01 -0700

    added some blogcard skels

---
+++ src/components/BlogCard.js
@@ -0,0 +1,17 @@
+const BlogCard = () => {
+    return (
+        <div>
+            <div>
+                left side
+                Title
+                <br/>
+                Snippet
+            </div>
+            <div>
+                img preview
+            </div>
+        </div>
+    )
+}
+
+export default BlogCard
