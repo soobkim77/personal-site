d94db290d45ddd6ff4b1733069a327001b7351f4
Soo-Beom <soobeomkim77@gmail.com>
2021-06-29 19:11:48 -0700

    added in initial blog

--- src/components/BlogCard.js
+++ src/components/BlogCard.js
@@ -1,16 +1,49 @@
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import { Link } from 'react-router-dom';
+
+const useStyles = makeStyles({
+    root: {
+      maxWidth: 345,
+    },
+    media: {
+      height: 140,
+    },
+  });
+
 const BlogCard = () => {
+    const classes = useStyles();
+
     return (
-        <div>
-            <div>
-                left side
-                Title
-                <br/>
-                Snippet
-            </div>
-            <div>
-                img preview
-            </div>
-        </div>
+
+    <Card className={classes.root}>
+      <CardActionArea>
+        {/* <CardMedia
+          className={classes.media}
+          image="/static/images/cards/contemplative-reptile.jpg"
+          title="Contemplative Reptile"
+        /> */}
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            Counting Primes?
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            Data Structure and Algorithms: Counting the primes numbers up to an inputted number.
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Link to="https://soobkim77.medium.com/counting-primes-361247a63504">
+          To Medium
+        </Link>
+      </CardActions>
+    </Card>
     )
 }
 
--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -2,6 +2,10 @@ const Blog = () => {
     return (
         <div>
             Blog
+
+            <div>
+                For more Blogs, please follow this <a target="_blank" href="https://soobkim77.medium.com/">link!</a>
+            </div>
         </div>
     )
 }
